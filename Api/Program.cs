37ac91d05ee7d2f57c012b07c9f234900f695f8f
Alexandre <alexandre.guerra.dev@gmail.com>
2026-05-29 13:44:01 -0300

    fix: injeção de services e repositories adicionado ao Program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,10 +1,15 @@
+using Api.Src.Application.Services;
 using Api.Src.Infraestructure.Database;
+using Api.Src.Infraestructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var databasePath = builder.Configuration["Database:Path"];
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(databasePath));
+builder.Services.AddScoped<TaskItemRepository>();
+
+builder.Services.AddScoped<TaskItemService>();
 
 builder.Services.AddControllers();
 
