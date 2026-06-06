a296b9df6b45109f381323b387de7de4fc4564d3
Alexandre <alexandre.guerra.dev@gmail.com>
2026-06-06 16:15:40 -0300

    feat(exceptions): classes base de exceções implementadas

--- Api/Src/Api/Middlewares/AppExceptionHandlerMiddleware.cs
+++ Api/Src/Api/Middlewares/AppExceptionHandlerMiddleware.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Src.Shared;
 
 namespace Api.Src.Api.Middlewares;
 
@@ -14,6 +15,26 @@ public class AppExceptionHandlerMiddleware(RequestDelegate next)
         try
         {
             await _next(context);
+        }
+        catch (BusinessException ex)
+        {
+            await WriteResponseAsync(context, StatusCodes.Status422UnprocessableEntity, ex.Message);
+        }
+        catch (ConflictException ex)
+        {
+            await WriteResponseAsync(context, StatusCodes.Status409Conflict, ex.Message);
+        }
+        catch (ForbidException ex)
+        {
+            await WriteResponseAsync(context, StatusCodes.Status403Forbidden, ex.Message);
+        }
+        catch (NotFoundException ex)
+        {
+            await WriteResponseAsync(context, StatusCodes.Status404NotFound, ex.Message);
+        }
+        catch (UnauthorizedException ex)
+        {
+            await WriteResponseAsync(context, StatusCodes.Status401Unauthorized, ex.Message);
         }
         catch (Exception ex)
         {
---
+++ Api/Src/Shared/NotFoundException.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Src.Shared;
+
+public abstract class NotFoundException(string message) : Exception(message);
